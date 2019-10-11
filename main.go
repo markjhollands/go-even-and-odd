6ac52a6c6ed0fcd7bcff9e66b394a3877c0c6216
Mark Hollands <markjhollands@gmail.com>
2019-10-11 14:10:46 +0100

    Solution to assignment

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	numbers := []int{}
+	for i := 0; i <= 10; i++ {
+		numbers = append(numbers, i)
+	}
+
+	for _, number := range numbers {
+		if number%2 != 0 {
+			fmt.Println(number, "is odd")
+		} else {
+			fmt.Println(number, "is even")
+		}
+	}
 }
